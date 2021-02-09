d851aa3d0dba86614bda2233d6b7c2f1202eb46a
eaina7 <eaina7gmail.com>
2021-02-09 21:32:19 +0100

    second structure

--- src/components/Poke.js
+++ src/components/Poke.js
@@ -1,7 +1,21 @@
 import axios from 'axios'
 
 const Poke = ({pokemon}) => {
-    return (<div>This is Poke</div>)
+    return (
+        <div className ='Poke'>
+        <h1>This is Poke</h1>
+           <p className='title'>Name</p>
+           <p>{pokemons.name}</p>
+           <p className='title'>Type</p>
+           <p>{pokemons.type}</p>
+           <p className='title'>Base</p>
+           <p>{pokemons.base}</p>
+           
+      </div>              
+              
+        
+        
+        )
 }
 
 export default Poke;
