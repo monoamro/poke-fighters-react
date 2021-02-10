0f9971d07f418a605407dcd075f95f2474846e09
eaina7 <eaina7gmail.com>
2021-02-10 15:31:44 +0100

    Poke component updated

--- src/components/Poke.js
+++ src/components/Poke.js
@@ -5,11 +5,19 @@ const Poke = ({pokemon}) => {
         <div className ='Poke'>
         <h1>This is Poke</h1>
            <p className='title'>Name</p>
-           <p>{pokemons.name}</p>
+           <p>{pokemon.name.english}</p>
            <p className='title'>Type</p>
-           <p>{pokemons.type}</p>
-           <p className='title'>Base</p>
-           <p>{pokemons.base}</p>
+           <p>Type: </p>
+      {pokemon.type.map((type) => {
+        return <p>{type}</p>;
+        
+      })}
+      <p>Attack: {pokemon.base.Attack}</p>
+      <p>Defense: {pokemon.base.Defense}</p>
+      <p>Hit points: {pokemon.base.HP}</p>
+      <p>Special Attack: {pokemon.base["Sp. Attack"]}</p>
+      <p>Special Defense: {pokemon.base["Sp. Defense"]}</p>
+      <p>Speed: {pokemon.base.Speed}</p>
            
       </div>              
               
