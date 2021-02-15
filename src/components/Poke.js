b7ebca99aba213f75256bfa9eaa6a350cd3d450f
Montaser Amro <mono.amro@gmail.com>
2021-02-15 19:00:44 +0200

    fixes loading the component

--- src/components/Poke.js
+++ src/components/Poke.js
@@ -8,8 +8,8 @@ import {useParams} from 'react-router-dom';
 import Api from '../Api';
 
 
-const Poke = ({pokemon}) => {
-    /*const [pokemon, setPokemon] = useState();
+const Poke = () => {
+    const [pokemon, setPokemon] = useState();
     const {id} = useParams();
     useEffect(() => {
       Api.getById(id)
@@ -19,56 +19,51 @@ const Poke = ({pokemon}) => {
       .catch(e => console.error(e))
     }, [id])
 
-    
-    console.log(pokemon)
-    */
-    return (
-      <div className = 'Poke'>
-      <Card border="primary"style={{ width: '30rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title> <p className='title_name'>Name</p>
-         <p>{pokemon.name.english}</p>
-         <p className='title_types'>Type</p></Card.Title>
-          <Card.Text>
-                  
-           
-               
-      <div className ='title_types'>
-        
-    {pokemon.type.map((type) => {
-      return <div className ='title_type'><p>{type}</p></div>;
+
+    return (pokemon ? <div className = 'Poke'>
+    <Card border="primary"style={{ width: '30rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title> <p className='title_name'>Name</p>
+       <p>{pokemon.name.english}</p>
+       <p className='title_types'>Type</p></Card.Title>
+        <Card.Text>
+                
+         
+             
+    <div className ='title_types'>
       
-    })}
-    </div>
-     <div className='title_info'>
-     <div className="title_data title_data--weight">
-    <p>Attack: {pokemon.base.Attack}</p>
-    </div>
-    <div className="title_data title_data--weight">
-    <p>Defense: {pokemon.base.Defense}</p>
-    </div>
-    <div className="title_data title_data--weight">
-    <p>Hit points: {pokemon.base.HP}</p>
-    </div>
-    <div className="title_data title_data--weight">
-    <p>Special Attack: {pokemon.base["Sp. Attack"]}</p>
-    </div>
-    <div className="title_data title_data--weight">
-    <p>Special Defense: {pokemon.base["Sp. Defense"]}</p>
-    </div>
-    <div className="title_data title_data--weight">
-    <p>Speed: {pokemon.base.Speed}</p>
-    </div>
-    </div>
+  {pokemon.type.map((type) => {
+    return <div className ='title_type'><p>{type}</p></div>;
     
-    
-    </Card.Text>
-    </Card.Body>
-    </Card>
-    </div>
-   
-        )
+  })}
+  </div>
+   <div className='title_info'>
+   <div className="title_data title_data--weight">
+  <p>Attack: {pokemon.base.Attack}</p>
+  </div>
+  <div className="title_data title_data--weight">
+  <p>Defense: {pokemon.base.Defense}</p>
+  </div>
+  <div className="title_data title_data--weight">
+  <p>Hit points: {pokemon.base.HP}</p>
+  </div>
+  <div className="title_data title_data--weight">
+  <p>Special Attack: {pokemon.base["Sp. Attack"]}</p>
+  </div>
+  <div className="title_data title_data--weight">
+  <p>Special Defense: {pokemon.base["Sp. Defense"]}</p>
+  </div>
+  <div className="title_data title_data--weight">
+  <p>Speed: {pokemon.base.Speed}</p>
+  </div>
+  </div>
+  
+  
+  </Card.Text>
+  </Card.Body>
+  </Card>
+  </div> : <h1>Loading pokemons</h1>)
 }
 
 export default Poke;
