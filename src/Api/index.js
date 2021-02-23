37d88881284ddf86143e06375d0d03da20f1cb50
Jasmin Jenull <jasminjenull@gmail.com>
2021-02-23 09:40:20 +0100

    Update base url for api calls

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-const baseUrl = "https://pokefighters.herokuapp.com";
-const pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon/'
+const baseUrl =
+  "http://ec2-18-185-96-249.eu-central-1.compute.amazonaws.com/api";
+const pokeApiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
 const Api = {
   getAll: () => axios.get(`${baseUrl}/pokemons`),
   getById: (id) => axios.get(`${baseUrl}/pokemons/${id}`),
   postFight: (requestBody) => axios.post(`${baseUrl}/fight`, requestBody),
-  getImage: (id) => axios.get(`${pokeApiUrl}${id}`)
+  getImage: (id) => axios.get(`${pokeApiUrl}${id}`),
 };
 
 export default Api;
